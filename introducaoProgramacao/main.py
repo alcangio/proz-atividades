e23109e7a6abd80161ca0cc210735e81971927ee
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-01-29 16:37:47 -0300

    2.0

--- introducaoProgramacao/main.py
+++ introducaoProgramacao/main.py
@@ -4,19 +4,15 @@ def calculadora(numero1, numero2, operacao):
     entrada que definirá a operação a ser executada.'''
     if operacao == "1":
         resultado = numero1 + numero2
-        return resultado
     elif operacao == "2":
         resultado = numero1 - numero2
-        return resultado
     elif operacao == "3":
         resultado = numero1 * numero2
-        return resultado
     elif operacao == "4":
         resultado = numero1 / numero2
-        return resultado
     else:
         resultado = 0
-        return resultado
+    return resultado
 
 
 numero1 = float(input("Digite o primeiro número: "))
